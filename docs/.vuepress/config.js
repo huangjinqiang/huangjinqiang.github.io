6a23466c649c98a5ae0cde565cd256cee9f6dc50
天明 <huangjinqiang@2980.com>
2021-05-24 00:50:01 +0800

    feat: 增加.vuepress/config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: 'Just Do It',
+  description: 'Just Do It',
+  head: [
+    ["link", { "rel": "icon", "href": "/favicon.png" }],
+    ["meta", { "name": "viewport", "content": "width=device-width,initial-scale=1,user-scalable=no" }],
+  ],
+  plugins: [
+    ['@vuepress/nprogress'],
+    ["dynamic-title", {
+      showIcon: "/favicon.png",
+      showText: "(๑•̀ㅂ•́)و✧ ",
+      hideIcon: "/favicon-blur.png",
+      hideText: "_____λ......___丬 别走啊～～",
+      recoverTime: 2000
+    }]
+  ],
+  themeConfig: {
+    logo: false,
+    nav: [
+      {
+        text: 'leetcode',
+        link: '/leetcode/'
+      }
+    ],
+    lastUpdated: 'Last Updated'
+  }
+}
